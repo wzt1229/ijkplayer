14a7d0df25ea538f8601e7f791f5d4609517195e
qianlongxu <qianlongxu@gmail.com>
2021-11-19 16:26:46 +0800

    修复编译错误

--- mac/IJKMediaDemo/IJKMediaDemo/RootViewController.m
+++ mac/IJKMediaDemo/IJKMediaDemo/RootViewController.m
@@ -193,8 +193,8 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ijkPlayerPreparedToPlay:) name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification object:self.player];
     
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:IJKMPMoviePlayerExTimedTextFirstLoadtNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ijkPlayerPreparedToPlay:) name:IJKMPMoviePlayerExTimedTextFirstLoadtNotification object:self.player];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:IJKMPMoviePlayerSelectedStreamDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ijkPlayerPreparedToPlay:) name:IJKMPMoviePlayerSelectedStreamDidChangeNotification object:self.player];
     
     self.player.scalingMode = IJKMPMovieScalingModeAspectFit;
     self.player.shouldAutoplay = YES;
