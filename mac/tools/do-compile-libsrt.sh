bbc05539e847b5f6b845049e2a7902cee55ee496
qianlongxu <qianlongxu@gmail.com>
2021-12-06 17:28:28 +0800

    解决 libsrt 编译错误

--- mac/tools/do-compile-libsrt.sh
+++ mac/tools/do-compile-libsrt.sh
@@ -80,7 +80,7 @@ FF_XCODE_BITCODE=
 if [[ "$FF_ARCH" == "x86_64" ]]; then
     FF_BUILD_NAME="libsrt-x86_64"
     FF_XCRUN_OSVERSION="-mmacosx-version-min=10.10"
-    LIBASS_CFG_FLAGS="--disable-dependency-tracking --disable-fontconfig $LIBSRT_CFG_FLAGS"
+    LIBASS_CFG_FLAGS="--disable-dependency-tracking --disable-debug $LIBSRT_CFG_FLAGS"
 else
     echo "unknown architecture $FF_ARCH";
     exit 1
@@ -131,7 +131,6 @@ cd $FF_BUILD_SOURCE
 if [ -f "./Makefile" ]; then
     echo 'reuse configure'
 else
-    autoreconf -i
     echo "config: $LIBSRT_CFG_FLAGS"
     ./configure \
         $LIBSRT_CFG_FLAGS
