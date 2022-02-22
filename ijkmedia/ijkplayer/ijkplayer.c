6ae274e5cee189823c08d5f83ef8f89a5e90c99d
Youshi Qian <youshiqian206918@sohu-inc.com>
2022-02-22 09:55:52 +0800

    fix ijkmp_load_external_subtitle typo,  ffp_set_ --> ffp_load

--- ijkmedia/ijkplayer/ijkplayer.c
+++ ijkmedia/ijkplayer/ijkplayer.c
@@ -831,7 +831,7 @@ int ijkmp_load_external_subtitle(IjkMediaPlayer* mp, const char* file_name)
 {
     assert(file_name);
     MPTRACE("ijkmp_load_external_subtitle(%s)\n", file_name);
-    int retval = ffp_set_external_subtitle(mp->ffplayer, file_name);
+    int retval = ffp_load_external_subtitle(mp->ffplayer, file_name);
     MPTRACE("ijkmp_load_external_subtitle()=%d\n", retval);
     return retval;
 }
