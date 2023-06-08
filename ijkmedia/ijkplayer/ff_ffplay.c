de9eb76329aea786c5a512d9bff0e3a4ef21e6b4
qianlongxu <qianlongxu@gmail.com>
2023-06-08 13:32:10 +0800

    sync ffplay: don't shadow global variable [117672c814]

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -881,13 +881,13 @@ static void check_external_clock_speed(VideoState *is) {
 }
 
 /* seek in the stream */
-static int stream_seek(VideoState *is, int64_t pos, int64_t rel, int seek_by_bytes)
+static int stream_seek(VideoState *is, int64_t pos, int64_t rel, int by_bytes)
 {
     if (!is->seek_req) {
         is->seek_pos = pos;
         is->seek_rel = rel;
         is->seek_flags &= ~AVSEEK_FLAG_BYTE;
-        if (seek_by_bytes)
+        if (by_bytes)
             is->seek_flags |= AVSEEK_FLAG_BYTE;
         is->seek_req = 1;
         is->viddec.start_seek_time = SDL_GetTickHR();
