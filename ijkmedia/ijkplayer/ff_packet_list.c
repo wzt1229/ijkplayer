bb838a2053c38ecda2ad454e7855fbfe62d7e9ce
qianlongxu <qianlongxu@gmail.com>
2023-06-08 10:52:17 +0800

    sync ffplay:switch to new FIFO API  [bdf9ed41fe]

--- ijkmedia/ijkplayer/ff_packet_list.c
+++ ijkmedia/ijkplayer/ff_packet_list.c
@@ -10,7 +10,7 @@
 int packet_queue_put_private(PacketQueue *q, AVPacket *pkt)
 {
     MyAVPacketList pkt1;
-
+    int ret;
     if (q->abort_request)
        return -1;
     
@@ -20,8 +20,9 @@ int packet_queue_put_private(PacketQueue *q, AVPacket *pkt)
     }
     pkt1.pkt = pkt;
     pkt1.serial = q->serial;
-    av_fifo_write(q->pkt_list, &pkt1, 1);
-    //av_fifo_generic_write(q->pkt_list, &pkt1, sizeof(pkt1), NULL);
+    ret = av_fifo_write(q->pkt_list, &pkt1, 1);
+    if (ret < 0)
+        return ret;
     q->nb_packets++;
     q->size += pkt1.pkt->size + sizeof(pkt1);
     q->duration += FFMAX(pkt1.pkt->duration, MIN_PKT_DURATION);
@@ -69,7 +70,7 @@ int packet_queue_put_nullpacket(PacketQueue *q, int stream_index)
 int packet_queue_init(PacketQueue *q)
 {
     memset(q, 0, sizeof(PacketQueue));
-    q->pkt_list = av_fifo_alloc2(50, sizeof(MyAVPacketList), 0);
+    q->pkt_list = av_fifo_alloc2(50, sizeof(MyAVPacketList), AV_FIFO_FLAG_AUTO_GROW);
     if (!q->pkt_list)
         return AVERROR(ENOMEM);
     //av_fifo_auto_grow_limit(q->pkt_list, MAX_MIN_FRAMES);
@@ -93,9 +94,7 @@ void packet_queue_flush(PacketQueue *q)
 
     SDL_LockMutex(q->mutex);
     
-    while (av_fifo_can_read(q->pkt_list) > 0) {
-        av_fifo_read(q->pkt_list, &pkt1, 1);
-        //av_fifo_generic_read(q->pkt_list, &pkt1, sizeof(pkt1), NULL);
+    while (av_fifo_read(q->pkt_list, &pkt1, 1) >= 0) {
         av_packet_free(&pkt1.pkt);
     }
     q->nb_packets = 0;
@@ -146,8 +145,7 @@ int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block, int *serial)
             break;
         }
         
-        if (av_fifo_can_read(q->pkt_list) > 0) {
-            av_fifo_read(q->pkt_list, &pkt1, 1);
+        if (av_fifo_read(q->pkt_list, &pkt1, 1) >= 0) {
             q->nb_packets--;
             q->size -= pkt1.pkt->size + sizeof(pkt1);
             q->duration -= FFMAX(pkt1.pkt->duration, MIN_PKT_DURATION);
