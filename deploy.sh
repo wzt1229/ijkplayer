a1a80189dbe19f87eea1f25f250843aef5a596a1
qianlongxu <qianlongxu@gmail.com>
2023-09-15 14:58:10 +0800

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# deploy lastest tag and develop branch to github.
+# deploy lastest tag and master branch to github.
 
-url=$(git remote get-url github)
+url=$(git remote get-url github >/dev/null)
 if [[ $? -ne 0 ]]; then
+    echo 'add github remote'
     git remote add github git@github.com:debugly/ijkplayer.git
 fi
 
 tag=$(git describe --abbrev=0)
-echo '=== will push develop branch to github ==='
-git push github develop
+echo '=== will push master branch to github ==='
+git push github master
 echo "=== will push ${tag} tag to github ==="
 git push github ${tag}
 
