a782330f361328ee836f48b2962c21a1f58539a2
qianlongxu <qianlongxu@gmail.com>
2024-06-13 10:29:18 +0800

    subtitle y-coordinate in [0-0.75] ignore adjust position

--- ijkmedia/ijkplayer/ff_subtitle_def_internal.h
+++ ijkmedia/ijkplayer/ff_subtitle_def_internal.h
@@ -9,8 +9,8 @@
 #define ff_subtitle_def_internal_hpp
 
 #include "ff_subtitle_def.h"
-//忽略向上移动的字幕范围 [0-0.8]
-#define SUBTITLE_MOVE_WATERMARK 0.8
+//忽略向上移动的字幕范围 [0-0.75]
+#define SUBTITLE_MOVE_WATERMARK 0.75
 
 FFSubtitleBuffer *ff_subtitle_buffer_alloc_rgba32(SDL_Rectangle rect);
 FFSubtitleBuffer *ff_subtitle_buffer_alloc_r8(SDL_Rectangle rect);
