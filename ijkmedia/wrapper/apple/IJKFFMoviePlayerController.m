61c4a96bb45d1af061a16eca024579ce2f8127d4
qianlongxu <qianlongxu@gmail.com>
2024-08-13 11:37:42 +0800

    Upgrade IJK Required FFmpeg Version

--- ijkmedia/wrapper/apple/IJKFFMoviePlayerController.m
+++ ijkmedia/wrapper/apple/IJKFFMoviePlayerController.m
@@ -42,7 +42,7 @@
 #include "../ijkmedia/ijkplayer/ijkmeta.h"
 #include "../ijkmedia/ijkplayer/ff_ffmsg_queue.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "n5.1.4-18"; //"n5.1.4-18-g68b3171";
+static const char *kIJKFFRequiredFFmpegVersion = "n6.1.1-19";
 
 static void (^_logHandler)(IJKLogLevel level, NSString *tag, NSString *msg);
 
