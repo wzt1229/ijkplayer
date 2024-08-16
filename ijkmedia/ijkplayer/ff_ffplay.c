d8a5ac06464d762e9ccd7d42045765ebd3438ed2
qianlongxu <qianlongxu@gmail.com>
2024-08-16 18:21:45 +0800

    update playable statistics after seek

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -3309,6 +3309,44 @@ static void reset_buffer_size(FFPlayer *ffp)
     av_log(NULL, AV_LOG_INFO, "auto decision max buffer size:%dMB\n",ffp->dcc.max_buffer_size/1024/1024);
 }
 
+static void update_playable_duration(FFPlayer *ffp)
+{
+    if (!ffp) {
+        return;
+    }
+    VideoState *is           = ffp->is;
+    if (!is) {
+        return;
+    }
+    int     cached_duration_in_ms = -1;
+    int64_t audio_cached_duration = -1;
+    int64_t video_cached_duration = -1;
+    
+    if (is->audio_st) {
+        audio_cached_duration = ffp->stat.audio_cache.duration;
+    }
+    
+    if (is->video_st) {
+        video_cached_duration = ffp->stat.video_cache.duration;
+    }
+    
+    if (video_cached_duration > 0 && audio_cached_duration > 0) {
+        cached_duration_in_ms = (int)IJKMIN(video_cached_duration, audio_cached_duration);
+    } else if (video_cached_duration > 0) {
+        cached_duration_in_ms = (int)video_cached_duration;
+    } else if (audio_cached_duration > 0) {
+        cached_duration_in_ms = (int)audio_cached_duration;
+    }
+    
+    if (cached_duration_in_ms >= 0) {
+        int64_t buf_time_position = ffp_get_current_position_l(ffp) + cached_duration_in_ms;
+        if (ffp->playable_duration_ms != buf_time_position) {
+            //av_log(ffp, AV_LOG_DEBUG, "set playable_duration_ms:%lld\n", buf_time_position);
+            ffp->playable_duration_ms = buf_time_position;
+        }
+    }
+}
+
 /* this thread gets the stream from the disk or the network */
 static int read_thread(void *arg)
 {
@@ -4837,7 +4875,7 @@ long ffp_get_playable_duration_l(FFPlayer *ffp)
     assert(ffp);
     if (!ffp)
         return 0;
-    //when read frame eof,the palyable duration is close to but less than the total duration,cause the palyable progress bar is never full.
+    //when read frame eof,the playable duration is close to but less than the total duration,cause the playable progress bar is never full.
     if (ffp->is && ffp->is->eof) {
         return ffp_get_duration_l(ffp);
     }
@@ -4900,6 +4938,13 @@ void ffp_toggle_buffering_l(FFPlayer *ffp, int buffering_on)
         av_log(ffp, AV_LOG_DEBUG, "ffp_toggle_buffering_l: end\n");
         is->buffering_on = 0;
         stream_update_pause_l(ffp);
+        
+        //when paused player,need update playable statistics after seek
+        if (is->pause_req) {
+            ffp_statistic_l(ffp);
+            update_playable_duration(ffp);
+        }
+        
         if (is->seek_buffering) {
             is->seek_buffering = 0;
             ffp_notify_msg2(ffp, FFP_MSG_BUFFERING_END, 1);
