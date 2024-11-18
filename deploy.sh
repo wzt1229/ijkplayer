5c3a983df5ae396580303fd8920de6c4d3522ce8
qianlongxu <qianlongxu@gmail.com>
2024-11-18 12:47:14 +0800

    deploy shell support branch

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # deploy lastest tag and master branch to github.
 
+branch="${1:-master}"
+
 url=$(git remote get-url github >/dev/null 2>&1)
+
 if [[ $? -ne 0 ]]; then
     echo 'add github remote'
     git remote add github git@github.com:debugly/ijkplayer.git
 fi
 
-echo '=== will getch github tags ==='
+echo '=== will fetch github tags ==='
 git fetch github --tag
-echo '=== will push master branch to github ==='
-git push github master
+echo "=== will push $branch branch to github ==="
+git push github $branch
 tag=$(git describe --abbrev=0 --tags)
 echo "=== latest tag is ${tag} ==="
 #git push github ${tag} --force
